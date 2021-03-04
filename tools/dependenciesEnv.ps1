7a2d8af00dbb50d42b6fa6a73f6a78b5c2a83419
Andrei Hutanu <andrei.hutanu@evalan.com>
2021-03-04 11:19:23 +0100

    updated to version 10.2.1

--- tools/dependenciesEnv.ps1
+++ tools/dependenciesEnv.ps1
@@ -1,1 +1,2 @@
-$GNUARMEMB_TOOLCHAIN_PATH = "$ENV:ChocolateyInstall\lib\gcc-arm-embedded\tools"
+$TOOLCHAIN_SUBDIR = "gcc-arm-none-eabi-10-2020-q4-major"
+$GNUARMEMB_TOOLCHAIN_PATH = "$ENV:ChocolateyInstall\lib\gcc-arm-embedded\tools\$TOOLCHAIN_SUBDIR"
